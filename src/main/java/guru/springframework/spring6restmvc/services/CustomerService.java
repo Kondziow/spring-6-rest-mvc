2a7082a9883d328eb3c96902226337cf3a51c0dd
Kondziow <konradwojtanowski02@gmail.com>
2023-10-22 20:15:25 +0200

    control commit

---
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerService.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.model.CustomerDTO;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CustomerService {
+    List<CustomerDTO> getAllCustomers();
+
+    Optional<CustomerDTO> getCustomerById(UUID id);
+
+    CustomerDTO saveNewCustomer(CustomerDTO customer);
+
+    void updateById(UUID customerId, CustomerDTO customer);
+
+    void deleteById(UUID customerId);
+
+    void patchCustomerById(UUID customerId, CustomerDTO customer);
+}
